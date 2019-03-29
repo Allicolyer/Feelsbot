8fbda54e0ebabf52729f9736a9c536c30ca30808
Alli Colyer <allicolyer@gmail.com>
2019-03-29 16:32:14 -0700

    use tweet embedding

--- src/components/TweetAccordion.js
+++ src/components/TweetAccordion.js
@@ -1,17 +1,14 @@
 import React from "react";
-import { Heading, Text } from "rebass";
 import { AccordionItem } from "react-light-accordion";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 
 const TweetAccordion = props => {
   if (!!props.rating.num) {
     return (
       <AccordionItem title={props.title}>
         {props.rating.tweets.map(tweet => (
-          <div>
-            <Heading as="h6">
-              <a href={tweet.url}>{tweet.user.screen_name}</a>
-            </Heading>
-            <Text fontSize={3}>{tweet.text}</Text>
+          <div class="tweet">
+            <TwitterTweetEmbed tweetId={`${tweet.id_str}`} />
           </div>
         ))}
       </AccordionItem>
