001eaf22830eaa954dd495db49a99bbd82406a24
Alli Colyer <allicolyer@gmail.com>
2019-06-15 19:23:01 -0700

    simply solution by using clientHeight property and make solution work for multiple page loads in a row

--- src/components/TweetGrid.js
+++ src/components/TweetGrid.js
@@ -1,68 +1,41 @@
 import React from "react";
 import StackGrid from "react-stack-grid";
-import TweetCard from "./TweetCard";
 import { TwitterTweetEmbed } from "react-twitter-embed";
-import ReactDOM from "react-dom";
 import { Text } from "./shared";
-
-const measureHeight = element =>
-  element ? ReactDOM.findDOMNode(element).offsetHeight : 0;
-
-const heightTimer = (element, grid) => {
-  let timerID = setInterval(() => {
-    measureHeight(element);
-    console.log("I'm still going");
-    if (measureHeight(element)) {
-      updateGrid(grid);
-      clearInterval(timerID);
-      console.log("I stop");
-    }
-  }, 500);
-  //stop automatically after 5 seconds
-  setTimeout(() => {
-    clearInterval(timerID);
-  }, 5000);
-};
-
-const updateGrid = grid => {
-  grid.updateLayout();
-};
 
 class TweetGrid extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { tweets: this.props.rating.tweets };
   }
 
-  componentWillMount() {
-    let findContent = setInterval(() => {
-      this.content && heightTimer(this.content, this.grid);
-      console.log("I trying");
-      if (this.content) {
-        //stop once it finds the content
-        clearInterval(findContent);
-        console.log("I find");
-      }
-    }, 200);
+  updateGrid = () => {
+    this.grid.updateLayout();
+  };
 
-    //stop automatically after 5 seconds
+  componentWillUpdate() {
+    let findelement = setInterval(() => {
+      let embeds = document.getElementsByClassName("twitter-tweet");
+      let height = embeds.length ? embeds[0].clientHeight : 0;
+      if (height) {
+        this.updateGrid();
+        clearInterval(findelement);
+      }
+    }, 500);
+
+    //stop automatically after 8 seconds if tweet embeds are not loading
     setTimeout(() => {
-      clearInterval(findContent);
-    }, 5000);
+      clearInterval(findelement);
+    }, 8000);
   }
 
   render() {
     if (this.props.rating) {
       return (
         <div id="stack-grid" display="none">
-          {/* <button onClick={() => this.updateGrid()}>hi</button> */}
           <StackGrid columnWidth={300} gridRef={grid => (this.grid = grid)}>
             {this.props.rating.tweets.map(tweet => (
               <div key={tweet.id_str}>
-                <TwitterTweetEmbed
-                  ref={r => (this.content = r)}
-                  tweetId={tweet.id_str}
-                />
+                <TwitterTweetEmbed tweetId={tweet.id_str} />
               </div>
             ))}
           </StackGrid>
